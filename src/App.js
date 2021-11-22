2e0e5a0cf5b51790e5111e8145d56dc9edf588e3
Melih Şahin <melihsahin24@gmail.com>
2021-11-22 22:10:19 +0300

    Update design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { useState } from 'react';
 import Button from './components/Button/button';
+import ClickVoice from './assets/audios/click.mp3';
+import EnterVoice from './assets/audios/enter.mp3';
 
 const App = () => {
     const [calc, setCalc] = useState('');
     const [result, setResult] = useState('');
+
+    const playAudio = (audio) => new Audio(audio).play();
 
     const operators = ['/', '*', '+', '-', '.'];
 
@@ -16,10 +20,16 @@ const App = () => {
             return;
         }
 
-        setCalc(calc + val);
+        playAudio(ClickVoice);
+
+        setCalc(calc + val)
 
         if (!operators.includes(val)) {
-            setResult(eval(calc + val).toString());
+            try {
+                setResult(eval(calc + val).toString());
+            }catch (e) {
+                return;
+            }
         }
     }
 
@@ -35,35 +45,62 @@ const App = () => {
         return digits;
     }
 
-    const calculate = () => setCalc(eval(calc).toString());
+    const calculate = () => {
+        if (calc === '') return setCalc('0');
 
-    const clear = () => {
+        if(operators.includes(calc.slice(-1))) {
+            setCalc('error');
+            setResult('error');
+        }
+
+        playAudio(EnterVoice);
+
+        try {
+            setCalc(eval(calc).toString());
+        }catch (e) {
+            return;
+        }
+    }
+
+    const clearAll = () => {
         if (calc === '') return;
+        playAudio(EnterVoice);
 
         setCalc('');
         setResult('');
     }
 
+    const clearLast = () => {
+        if (calc === '') return;
+        playAudio(EnterVoice);
+
+        setCalc(calc.slice(0, -1));
+        setResult(calc.slice(0, -1));
+    }
+
     return (
         <div className="App">
             <div className="calculator">
+                <div className="outher-edge"/>
                 <div className="display">
                     { result ? <span>({ result })</span> : ''}
                     { calc || '0' }
                 </div>
                 <div className="operators">
                     <Button handleClick={ () => updateCalc('/')} value="/"/>
-                    <Button handleClick={ () => updateCalc('*')} value="*"/>
+                    <Button handleClick={ () => updateCalc('*')} value="x"/>
                     <Button handleClick={ () => updateCalc('+')} value="+"/>
                     <Button handleClick={ () => updateCalc('-')} value="-"/>
-                    <Button handleClick={ clear } value="DEL"/>
+                    <Button _class="clear-btn" handleClick={ clearLast } value="⬅"/>
+                    <Button _class="clear-btn" handleClick={ clearAll } value="C"/>
                 </div>
                 <div className="digits">
                     {createDigits()}
                     <Button handleClick={ () => updateCalc('0') } value= "0"/>
                     <Button handleClick={ () => updateCalc('.') } value="."/>
-                    <Button handleClick={ () => calculate() } value="="/>
+                    <Button _class="enter-btn" handleClick={ () => calculate() } value="="/>
                 </div>
+                <div className="outher-edge"/>
             </div>
         </div>
     );
--- src/components/Button/button.js
+++ src/components/Button/button.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const Button = ({ value, handleClick, _class, _key = null }) => {
   return (
